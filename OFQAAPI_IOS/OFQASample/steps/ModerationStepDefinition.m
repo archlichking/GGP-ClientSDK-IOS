8191a98d5eb481c3ba94630e567477f7f1de1c00
Lei Zhu <thunderzhulei@gmail.com>
2012-04-10 15:49:56 +0800

    initialize moderation branch

---
+++ OFQAAPI_IOS/OFQASample/steps/ModerationStepDefinition.m
@@ -0,0 +1,13 @@
+//
+//  ModerationStepDefinition.m
+//  OFQAAPI
+//
+//  Created by lei zhu on 4/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ModerationStepDefinition.h"
+
+@implementation ModerationStepDefinition
+
+@end
