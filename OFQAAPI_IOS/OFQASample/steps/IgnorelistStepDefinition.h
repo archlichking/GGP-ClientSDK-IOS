c8fb8ff8a100c880e8d436cd9d1a6b6d2ec19c18
Lei Zhu <thunderzhulei@gmail.com>
2012-05-18 17:41:02 -0700

    add more cases for ignorelist

--- OFQAAPI_IOS/OFQASample/steps/IgnorelistStepDefinition.h
+++ OFQAAPI_IOS/OFQASample/steps/IgnorelistStepDefinition.h
@@ -17,9 +17,15 @@
 
 - (void) I_load_my_ignore_list;
 
+- (void) I_load_first_page_of_my_ignore_list;
+
 - (void) my_ignore_list_should_be_size_of_PARAMINT:(NSString*) size;
 
-- (void) my_ignore_list_PARAM:(NSString*) isInclude 
+- (void) my_ignore_list_should_PARAM:(NSString*) isInclude 
                   _user_PARAM:(NSString*) user;
+
+- (void) I_check_user_from_my_ignore_list_with_id_PARAMINT:(NSString*) userid;
+
+- (void) status_of_PARAM:(NSString*) userid _in_my_ignore_list_should_be_PARAM:(NSString*) isInclude;
 
 @end
