9063ddf13d593b1ec24c6ac699d7cea6c65d3911
Lei Zhu <thunderzhulei@gmail.com>
2012-08-01 15:18:36 +0800

    local notification demo

--- OFQAAPI_IOS/OFQASample/steps/NotificationStepDefinition.h
+++ OFQAAPI_IOS/OFQASample/steps/NotificationStepDefinition.h
@@ -11,6 +11,7 @@
 
 @interface NotificationStepDefinition : StepDefinition
 
+//--- begin -------- notification
 - (void) I_set_notification_with_type_PARAM:(NSString*) type 
                          _and_message_PARAM:(NSString*) message 
                      _and_duration_PARAMINT:(NSString*) period;
@@ -21,7 +22,20 @@
 - (void) I_push_notification;
 
 - (void) notification_queue_should_have_notifications_of_size_PARAMINT:(NSString*) size;
-
 - (void) notification_queue_should_have_notification_with_message_PARAM:(NSString*) message;
+
+//--- end ----------- notification
+//--- begin --------- local notificaiton
+
+- (void) I_initialize_local_notification_with_TITLE_PARAM:(NSString*) title 
+                                          _and_BODY_PARAM:(NSString*) body 
+                                      _and_BARTITLE_PARAM:(NSString*) bartitle 
+                                   _and_INTERVAL_PARAMINT:(NSString*) intv 
+                                            _and_ID_PARAM:(NSString*) lnid 
+                                 _and_CALLBACKPARAM_PARAM:(NSString*) param;
+
+- (void) I_push_local_notification;
+
+//--- end ----------- local notification
 
 @end
