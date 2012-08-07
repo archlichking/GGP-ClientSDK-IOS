a345585fb459d435a2edaec4bc055c0080fc6115
Lei Zhu <thunderzhulei@gmail.com>
2012-08-07 11:17:33 +0800

    init jskit infrastructure by adding log demo

---
+++ OFQAAPI_IOS/OFQASample/steps/jskit/LogJsKitStepDefinition.h
@@ -0,0 +1,13 @@
+//
+//  LogJsKitStepDefinition.h
+//  OFQAAPI
+//
+//  Created by lei zhu on 8/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "StepDefinition.h"
+
+@interface LogJsKitStepDefinition : StepDefinition
+
+@end
---
+++ OFQAAPI_IOS/OFQASample/steps/jskit/LogJsKitStepDefinition.m
@@ -0,0 +1,13 @@
+//
+//  LogJsKitStepDefinition.m
+//  OFQAAPI
+//
+//  Created by lei zhu on 8/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "LogJsKitStepDefinition.h"
+
+@implementation LogJsKitStepDefinition
+
+@end
